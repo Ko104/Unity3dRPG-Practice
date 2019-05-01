2db0c2f91d327600015ac5e89b27181d2394ce12
IsaacCalligeros <45556525+IsaacCalligeros@users.noreply.github.com>
2019-05-01 22:29:14 +0930

    Basic Camera/Character Controller
    Initial Commit, Testing MetaData fields/gitignore/buildability.

---
+++ Trial1/Assets/Scripts/CameraController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public Transform target;
+    public float lookSmooth = 0.1f;
+    public Vector3 offsetFromTarget = new Vector3(0, 6, -8);
+    public float xTilt = 10;
+
+    Vector3 destination = Vector3.zero;
+    CharacterController charController;
+    float rotateVel = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetCameraTarget(target);
+    }
+
+    public void SetCameraTarget(Transform t)   // can point to any Transform, call from external objects etc
+    {
+        target = t;
+        if(t != null)
+        {
+            if (target.GetComponent<CharacterController>())
+            {
+                charController = target.GetComponent<CharacterController>();
+            }
+            else
+                Debug.Log("Camera Target needs Character Controller");
+        }
+        else
+            Debug.Log("No Camera Target");
+    }
+
+    void LateUpdate()   // Called Post Update calls ensures movements and rotations are based on current character movements
+    {
+        MoveToTarget();
+        LookAtTarget();
+    }
+
+    void MoveToTarget()
+    {
+        destination = charController.TargetRotation * offsetFromTarget;
+        destination += target.position;
+        transform.position = destination;
+    }
+
+    void LookAtTarget()
+    {
+        float eulerYAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, target.eulerAngles.y, ref rotateVel, lookSmooth);
+        transform.rotation = Quaternion.Euler(transform.eulerAngles.x, eulerYAngle, 0);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Trial1/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float inputDelay = 0.1f;
+    public float forwardVel = 12;
+    public float rotateVel = 100;
+    Quaternion targetRotation;
+    Rigidbody rBody;
+    float forwardInput, turnInput;
+
+    public Quaternion TargetRotation
+    {
+        get { return targetRotation; }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRotation = transform.rotation;
+        if (GetComponent<Rigidbody>())
+            rBody = GetComponent<Rigidbody>();
+        else
+            Debug.LogError("No RigidBody");
+        forwardInput = turnInput = 0;
+    }
+
+    void GetInput()
+    {
+        forwardInput = Input.GetAxis("Vertical");
+        turnInput = Input.GetAxis("Horizontal");
+    }
+
+    void run()
+    {
+        if (Mathf.Abs(forwardInput) > inputDelay)
+        {
+            rBody.velocity = transform.forward * forwardInput * forwardVel;
+        }
+        else
+        {
+            //zero Velocity
+            rBody.velocity = Vector3.zero;
+        }
+    }
+
+    void Turn()
+    {
+        if (Mathf.Abs(turnInput) > inputDelay)
+        {
+            targetRotation *= Quaternion.AngleAxis(rotateVel * turnInput * Time.deltaTime, Vector3.up);
+        }
+        transform.rotation = targetRotation;
+    }
+
+    private void FixedUpdate()
+    {
+        run();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+        Turn();
+    }
+
+}
